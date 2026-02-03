626b998009ee79702848078c72eaacf3fe065bc8
Mikelele <d.michaelthomasbennett@gmail.com>
2026-02-03 17:38:00 +0100

    Auto-sync: Database rebuilt & old CSVs archived/removed

--- maps_decks.py
+++ maps_decks.py
@@ -279,7 +279,7 @@ decks_map = {
                 {"qty": 1, "code": "SCD - 270", "section": "main"},
             ],
             "Makeshift Munitions": [
-                {"qty": 3, "code": "CMR - 191", "section": "main"},
+                {"qty": 1, "code": "CMR - 191", "section": "main"},
             ],
             "Bojuka Bog": [
                 {"qty": 1, "code": "BRC - 176", "section": "main"},
@@ -294,32 +294,358 @@ decks_map = {
                 {"qty": 4, "code": "MH3 - 232", "section": "main"},
             ],
             "Mountain": [
-                {"qty": 4, "code": "FBB - 290", "section": "main"},
+                {"qty": 1, "code": "FEFBB - 222", "section": "main"},
             ],
             "Swamp": [
-                {"qty": 4, "code": "MH2 - 256", "section": "main"},
+                {"qty": 3, "code": "FEFBB - 311", "section": "main"},
             ],
-            "Twisted Landscape": [
-                {"qty": 4, "code": "MH3 - 232", "section": "main"},
+            "Forest": [
+                {"qty": 1, "code": "FEFBB - 127", "section": "main"},
+                {"qty": 1, "code": "FEFBB - 128", "section": "main"},
+            ],
+            "Vault of Whispers": [
+                {"qty": 2, "code": "MRD - 286", "section": "main"},
             ],
             "Faerie Macabre": [
                 {"qty": 1, "code": "SHM - 66", "section": "sideboard"}
             ],
+            "Breath Weapon": [
+                {"qty": 2, "code": "CLB - 165", "section": "sideboard"}
+            ],
+            "Red Elemental Blast": [
+                {"qty": 4, "code": "3E - 170", "section": "sideboard"}
+            ],
+            "Troublemaker Ouphe": [
+                {"qty": 1, "code": "WOE - 194", "section": "sideboard"}
+            ],
+            "Weather the Storm": [
+                {"qty": 4, "code": "H1R - 24", "section": "sideboard"},
+            ],
+            "Duress": [
+                {"qty": 3, "code": "M10 - 96", "section": "sideboard"}
+            ],
+        }
+    },
+    "Mono U Snakes": {
+        "priority": 5,
+        "cards": {
+            "Cryptic Serpent": [
+                {"qty": 4, "code": "CMM - 83", "section": "main"},
+            ],
+            "Tolarian Terror": [
+                {"qty": 4, "code": "DMU - 72", "section": "main"},
+            ],
+            "Delver of Secrets // Insectile Aberration": [
+                {"qty": 4, "code": "ISD - 51", "section": "main"},
+            ],
+            "Brainstorm": [
+                {"qty": 4, "code": "SS1 - 3", "section": "main"},
+            ],
+            "Counterspell": [
+                {"qty": 4, "code": "IA - 64", "section": "main"},
+            ],
+            "Mental Note": [
+                {"qty": 4, "code": "JUD - 46", "section": "main"},
+            ],
+            "Thought Scour": [
+                {"qty": 4, "code": "IMA - 76", "section": "main"},
+            ],
+            "Lórien Revealed": [
+                {"qty": 4, "code": "LTR - 60", "section": "main"},
+            ],
+            "Ponder": [
+                {"qty": 3, "code": "LRW - 79", "section": "main"},
+            ],
+            "Sleep of the Dead": [
+                {"qty": 2, "code": "THB - 66", "section": "main"},
+            ],
+            "Spell Pierce": [
+                {"qty": 2, "code": "XLN - 81", "section": "main"},
+            ],
+            "Consider": [
+                {"qty": 1, "code": "MKC - 98", "section": "main"},
+            ],
+            "Deem Inferior": [
+                {"qty": 2, "code": "MH3 - 57", "section": "main"},
+            ],
+            "Deep Analysis": [
+                {"qty": 2, "code": "EMA - 45", "section": "main"},
+            ],
+            "Snow-Covered Island": [
+                {"qty": 16, "code": "MH1 - 251", "section": "main"},
+            ],
+            "Murmuring Mystic": [
+                {"qty": 3, "code": "CMM - 107", "section": "sideboard"}
+            ],
+            "Annul": [
+                {"qty": 4, "code": "KHM - 42", "section": "sideboard"}
+            ],
+            "Gut Shot": [
+                {"qty": 2, "code": "MM2 - 117", "section": "sideboard"},
+                {"qty": 2, "code": "NPH - 86", "section": "sideboard"}
+            ],
+            "Hydroblast": [
+                {"qty": 4, "code": "IA - 72", "section": "sideboard"}
+            ],
+        }
+    },
+    "Mono U Faeries": {
+        "priority": 6,
+        "cards": {
+            "Faerie Miscreant": [
+                {"qty": 4, "code": "ORI - 57", "section": "main"},
+            ],
+            "Faerie Seer": [
+                {"qty": 4, "code": "MH1 - 51", "section": "main"},
+            ],
+            "Humbling Elder": [
+                {"qty": 4, "code": "tdm - 48", "section": "main"},
+            ],
+            "Moon-Circuit Hacker": [
+                {"qty": 4, "code": "NEO - 334", "section": "main"},
+            ],
+            "Counterspell": [
+                {"qty": 4, "code": "IA - 64", "section": "main"},
+            ],
+            "Ninja of the Deep Hours": [
+                {"qty": 4, "code": "j25 - 108", "section": "main"},
+            ],
+            "Spellstutter Sprite": [
+                {"qty": 4, "code": "LRW - 89", "section": "main"},
+            ],
+            "Harrier Strix": [
+                {"qty": 2, "code": "OTJ - 52", "section": "main"},
+            ],
+            "Saiba Cryptomancer": [
+                {"qty": 1, "code": "MOM - 76", "section": "main"},
+            ],
+            "Force Spike": [
+                {"qty": 2, "code": "DDJ - 14", "section": "main"},
+            ],
+            "Spell Pierce": [
+                {"qty": 2, "code": "XLN - 81", "section": "main"},
+            ],
+            "Snap": [
+                {"qty": 3, "code": "DMR - 66", "section": "main"},
+            ],
+            "Of One Mind": [
+                {"qty": 4, "code": "IKO - 60", "section": "main"},
+            ],
+            "Island": [
+                {"qty": 18, "code": "ELD - 257", "section": "main"},
+            ],
+            "Steel Sabotage": [
+                {"qty": 1, "code": "MBS - 33", "section": "sideboard"},
+            ],
+            "Annul": [
+                {"qty": 3, "code": "KHM - 42", "section": "sideboard"},
+            ],
+            "Dispel": [
+                {"qty": 1, "code": "RTR - 36", "section": "sideboard"},
+            ],
+            "Blue Elemental Blast": [
+                {"qty": 1, "code": "2U - 50", "section": "sideboard"},
+                {"qty": 3, "code": "4E - 63", "section": "sideboard"},
+            ],
+            "Relic of Progenitus": [
+                {"qty": 3, "code": "ALA - 218", "section": "sideboard"}
+            ],
+            "Bind the Monster": [
+                {"qty": 2, "code": "KHM - 48", "section": "sideboard"}
+            ],
+            "Unable to Scream": [
+                {"qty": 1, "code": "DSK - 78", "section": "sideboard"}
+            ],
+        }
+    },
+    "Mono W Weenie": {
+        "priority": 7,
+        "cards": {
+            "Dawnbringer Cleric": [
+                {"qty": 1, "code": "AFR - 9", "section": "main"},
+            ],
+            "Eagles of the North": [
+                {"qty": 1, "code": "LTR - 7", "section": "main"},
+            ],
+            "Kor Skyfisher": [
+                {"qty": 4, "code": "ZEN - 23", "section": "main"},
+            ],
+            "Lunarch Veteran // Luminous Phantom": [
+                {"qty": 2, "code": "inr - 450", "section": "main"},
+            ],
+            "Novice Inspector": [
+                {"qty": 4, "code": "MKM - 29", "section": "main"},
+            ],
+            "Raffine's Informant": [
+                {"qty": 4, "code": "SNC - 26", "section": "main"},
+            ],
+            "Salt Road Packbeast": [
+                {"qty": 2, "code": "tdm - 23", "section": "main"},
+            ],
+            "Thraben Inspector": [
+                {"qty": 4, "code": "inr - 45", "section": "main"},
+            ],
+            "Spider-Man, Web-Slinger": [
+                {"qty": 3, "code": "spm - 16", "section": "main"},
+            ],
+            "Summon: Choco/Mog": [
+                {"qty": 3, "code": "fin - 35", "section": "main"},
+            ],
+            "Guardians' Pledge": [
+                {"qty": 1, "code": "M12 - 22", "section": "main"},
+            ],
+            "Prismatic Strands": [
+                {"qty": 3, "code": "JUD - 18", "section": "main"},
+            ],
+            "Thraben Charm": [
+                {"qty": 3, "code": "MH3 - 45", "section": "main"},
+            ],
+            "Battle Screech": [
+                {"qty": 4, "code": "CMM - 17", "section": "main"},
+            ],
+            "Greatsword of Tyr": [
+                {"qty": 1, "code": "CLB - 22", "section": "main"},
+            ],
+            "Snow-Covered Plains": [
+                {"qty": 18, "code": "MH1 - 250", "section": "main"},
+            ],
+            "Idyllic Grange": [
+                {"qty": 2, "code": "ELD - 246", "section": "main"},
+            ],
+            "Dust to Dust": [
+                {"qty": 4, "code": "DK - 78", "section": "sideboard"},
+            ],
+            "Destroy Evil": [
+                {"qty": 1, "code": "j25 - 189", "section": "sideboard"},
+            ],
+            "Journey to Nowhere": [
+                {"qty": 1, "code": "ZEN - 14", "section": "sideboard"}
+            ],
             "Relic of Progenitus": [
                 {"qty": 2, "code": "ALA - 218", "section": "sideboard"}
             ],
-            "Hydroblast": [
-                {"qty": 3, "code": "IA - 72", "section": "sideboard"}
+            "Revoke Existence": [
+                {"qty": 1, "code": "THB - 34", "section": "sideboard"}
             ],
-            "Vitu-Ghazi Inspector": [
-                {"qty": 3, "code": "MKM - 183", "section": "sideboard"}
+            "Palace Sentinels": [
+                {"qty": 2, "code": "CMM - 48", "section": "sideboard"}
             ],
-            "Scattershot Archer": [
-                {"qty": 4, "code": "CON - 90", "section": "sideboard"}
+            "Arashin Cleric": [
+                {"qty": 2, "code": "FRF - 5", "section": "sideboard"}
             ],
-            "Pulse of Murasa": [
-                {"qty": 2, "code": "M20 - 189", "section": "sideboard"}
+            "Standard Bearer": [
+                {"qty": 2, "code": "AP - 18", "section": "sideboard"}
             ],
         }
-    }
+    },
+    "Ruby Storm": {
+        "priority": 8,
+        "cards": {
+            "Dawnbringer Cleric": [
+                {"qty": 1, "code": "AFR - 9", "section": "main"},
+            ],
+            "Eagles of the North": [
+                {"qty": 1, "code": "LTR - 7", "section": "main"},
+            ],
+            "Kor Skyfisher": [
+                {"qty": 4, "code": "ZEN - 23", "section": "main"},
+            ],
+            "Lunarch Veteran // Luminous Phantom": [
+                {"qty": 2, "code": "inr - 450", "section": "main"},
+            ],
+            "Novice Inspector": [
+                {"qty": 4, "code": "MKM - 29", "section": "main"},
+            ],
+            "Raffine's Informant": [
+                {"qty": 4, "code": "SNC - 26", "section": "main"},
+            ],
+            "Salt Road Packbeast": [
+                {"qty": 2, "code": "tdm - 23", "section": "main"},
+            ],
+            "Thraben Inspector": [
+                {"qty": 4, "code": "inr - 45", "section": "main"},
+            ],
+            "Spider-Man, Web-Slinger": [
+                {"qty": 3, "code": "spm - 16", "section": "main"},
+            ],
+            "Summon: Choco/Mog": [
+                {"qty": 3, "code": "fin - 35", "section": "main"},
+            ],
+            "Guardians' Pledge": [
+                {"qty": 1, "code": "M12 - 22", "section": "main"},
+            ],
+            "Prismatic Strands": [
+                {"qty": 3, "code": "JUD - 18", "section": "main"},
+            ],
+            "Thraben Charm": [
+                {"qty": 3, "code": "MH3 - 45", "section": "main"},
+            ],
+            "Battle Screech": [
+                {"qty": 4, "code": "CMM - 17", "section": "main"},
+            ],
+            "Greatsword of Tyr": [
+                {"qty": 1, "code": "CLB - 22", "section": "main"},
+            ],
+            "Snow-Covered Plains": [
+                {"qty": 18, "code": "MH1 - 250", "section": "main"},
+            ],
+            "Idyllic Grange": [
+                {"qty": 2, "code": "ELD - 246", "section": "main"},
+            ],
+            "Dust to Dust": [
+                {"qty": 4, "code": "DK - 78", "section": "sideboard"},
+            ],
+            "Destroy Evil": [
+                {"qty": 1, "code": "j25 - 189", "section": "sideboard"},
+            ],
+            "Journey to Nowhere": [
+                {"qty": 1, "code": "ZEN - 14", "section": "sideboard"}
+            ],
+            "Relic of Progenitus": [
+                {"qty": 2, "code": "ALA - 218", "section": "sideboard"}
+            ],
+            "Revoke Existence": [
+                {"qty": 1, "code": "THB - 34", "section": "sideboard"}
+            ],
+            "Palace Sentinels": [
+                {"qty": 2, "code": "CMM - 48", "section": "sideboard"}
+            ],
+            "Arashin Cleric": [
+                {"qty": 2, "code": "FRF - 5", "section": "sideboard"}
+            ],
+            "Standard Bearer": [
+                {"qty": 2, "code": "AP - 18", "section": "sideboard"}
+            ],
+        }
+    },
 }
+
+
+for deck_name, deck_info in decks_map.items():
+
+    deck_main_total = 0
+    deck_side_total = 0    
+    # Access the 'cards' dictionary
+    cards_dict = deck_info.get("cards", {})
+    
+    for card_name, versions in cards_dict.items():
+        # 'versions' is the list: [{"qty": 4, "code": "..."}]
+        for version in versions:
+
+            qty = version.get('qty', 0)
+            code = version.get('code', '')
+
+            if version['section'] == 'main':
+                deck_main_total += version['qty']
+            elif version['section'] == 'sideboard':
+                deck_side_total += version['qty']
+
+            
+            if ' - ' not in code:
+                print(f"❌ CODE ERROR: [{deck_name}] {card_name} has a comma in code: '{code}'")
+
+    
+    if deck_main_total != 60 or deck_side_total != 15:
+        print(f"{deck_name} -> Main: {deck_main_total}, Side: {deck_side_total}")
+    else:
+        print(f"{deck_name} OK")
